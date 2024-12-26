f8ebe98c79ee4c2e137ea9758fd326c3836be069
Rémy Mondi <remy@mondi.dev>
2024-12-26 15:20:10 +0100

    [Add] Analytics + Speed insigths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +19,8 @@ export default function RootLayout({
         className=""
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
